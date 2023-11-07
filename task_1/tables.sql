4c7bd32c6e8d17756046b2a02981f9890aaf81b7
timbakkenes <tim.bakkenes@icloud.com>
2023-11-07 15:01:15 +0100

    tables

--- task_1/tables.sql
+++ task_1/tables.sql
@@ -1,8 +1,80 @@
-CREATE TABLE Branches (name TEXT, program TEXT);
+CREATE TABLE Branches (
+    name TEXT, 
+    program TEXT,
+    PRIMARY KEY (name, program)
+    );
 
-CREATE TABLE Students(idnr TEXT, name TEXT, login TEXT, program TEXT);
+CREATE TABLE Students(
+    idnr TEXT, 
+    name TEXT, 
+    login TEXT, 
+    program TEXT,
+    PRIMARY KEY (idnr)
+    );
 
-CREATE TABLE Courses(code TEXT, name TEXT, credits REAL, department TEXT);
+CREATE TABLE Courses(
+    code TEXT, 
+    name TEXT, 
+    credits REAL, 
+    department TEXT,
+    PRIMARY KEY (code)
+    );
 
-LimitedCourses(code, capacity)
-code → Courses.code
+CREATE TABLE LimitedCourses(
+    code TEXT, 
+    capacity INT, 
+    PRIMARY KEY (code),
+    FOREIGN KEY (code) REFERENCES Courses(code)
+    );
+
+CREATE TABLE StudentBranches(
+    student TEXT, 
+    branch TEXT, 
+    program TEXT,
+    PRIMARY KEY (student),
+    FOREIGN KEY (student) REFERENCES Students(idnr),
+    FOREIGN KEY (branch, program) REFERENCES Branches(name, program)
+    );
+
+CREATE TABLE Classifications(
+    name TEXT,
+    PRIMARY KEY (name)
+    );
+
+CREATE TABLE Classified(
+    student TEXT, 
+    branch TEXT, 
+    program TEXT,  
+    PRIMARY KEY (student), 
+    FOREIGN KEY (branch, program) REFERENCES Branches(name, program));
+
+CREATE TABLE MandatoryBranch(
+    course TEXT, 
+    branch TEXT, 
+    program TEXT, 
+    PRIMARY KEY (course, branch, program), 
+    FOREIGN KEY (course) REFERENCES Courses(code),
+    FOREIGN KEY (branch, program) REFERENCES Branches(name, program));
+
+CREATE TABLE RecommendedBranch(
+    course TEXT, 
+    branch TEXT, 
+    program TEXT, 
+    PRIMARY KEY (course, branch, program), 
+    FOREIGN KEY (course) REFERENCES Courses(code),
+    FOREIGN KEY (branch, program) REFERENCES Branches(name, program));
+
+CREATE TABLE Registerd(
+    student TEXT, 
+    course TEXT,  
+    PRIMARY KEY (student, course), 
+    FOREIGN KEY (student) REFERENCES Students(idnr),
+    FOREIGN KEY (course) REFERENCES LimitedCourses(code));
+
+CREATE TABLE Taken(
+    student TEXT, 
+    course TEXT, 
+    grade TEXT, 
+    PRIMARY KEY (student, course), 
+    FOREIGN KEY (student) REFERENCES Students(idnr),
+    FOREIGN KEY (course) REFERENCES LimitedCourses(code));
