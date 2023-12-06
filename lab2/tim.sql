a585bbf64a6dff52a31f95e322cfc673689e024e
axelg <axelgust@chalmers.se>
2023-12-06 10:16:55 +0100

    jdjd

--- lab2/tim.sql
+++ lab2/tim.sql
@@ -1,15 +1,25 @@
 CREATE FUNCTION course_filled() RETURNS trigger AS $course_filled$
-    BEGIN
+   DECLARE cap INT;
+   DECLARE reg INT;
+   BEGIN
+        SELECT COUNT(*)
 
-        IF (EXISTS (SELECT * FROM Registered WHERE course=NEW.course AND )) 
-        THEN 
-            RAISE EXCEPTION 'test2';
-        END IF;
+       IF (EXISTS (SELECT * FROM Registrations WHERE course=NEW.course AND student = New.student))
+       THEN
+           RAISE EXCEPTION 'test2';
+       ELSE
+       INSERT INTO Registered VALUES (New.student, New.course);
+       END IF;
+       
+
+       IF (EXISTS (SELECT unlocking FROM Prerequisites WHERE (unlocked = NEW.course) AND unlocking NOT IN (SELECT course FROM TAKEN WHERE (student = NEW.student))))
+       THEN
+            RAISE EXCEPTION 'error1';
+        
         RETURN NEW;
-    END;
+   END;
 $course_filled$ LANGUAGE plpgsql;
 
-CREATE TRIGGER course_filled  
-    INSTEAD OF INSERT ON Registrations
-    FOR EACH ROW EXECUTE PROCEDURE course_filled();
-
+CREATE TRIGGER course_filled 
+   INSTEAD OF INSERT ON Registrations
+   FOR EACH ROW EXECUTE PROCEDURE course_filled();
